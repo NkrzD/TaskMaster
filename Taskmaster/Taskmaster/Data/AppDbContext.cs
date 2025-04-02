7b5b982df2561bf5d54848b17f2154a0e98f1544
NkrzD <adri1959@hotmail.fr>
2025-04-02 12:26:27 +0200

    setup du projet

---
+++ Taskmaster/Taskmaster/Data/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Taskmaster.Data
+//{
+//    internal class AppDbContext
+//    {
+//    }
+//}
+
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskManagerApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql("Server=localhost;Port=3306;Database=taskmanagerdb;User=root;Password=;",
+                    new MySqlServerVersion(new Version(8, 0, 30)));
+            }
+        }
+    }
+}
+
